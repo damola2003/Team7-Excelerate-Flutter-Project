6b93ed7b82a1b5d5c2fa35845c1f7a0b58c0dd6a
damola2003 <otolorinoludamola@gmail.com>
2026-01-20 23:17:49 +0100

    update to the log in screen

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,26 +1,6 @@
 //This is the login page of the application
+
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Learn Sphere',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'Inter',
-      ),
-      home: const LoginPage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
