d0bda5a7ba196de8c0d666a76560a9c7e221894a
damola2003 <otolorinoludamola@gmail.com>
2026-01-21 00:20:13 +0100

    product listin

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'LearnSphere',
+
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
       home: const ProgramListingScreen(),
