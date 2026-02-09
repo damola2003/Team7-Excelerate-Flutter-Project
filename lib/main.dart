e07591e244d12a0bc4130fe3af58c73f1b93b8ca
Mxoliswa Kingsley Maamogwa <110988263+mxolisi07@users.noreply.github.com>
2026-02-09 15:22:35 +0200

    The newly updated code with the integration of firebase database functioning well, yet to update the email verification setup. It takes long to reach the end point of the user

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
+import 'package:excelerate/firebase_options.dart';
 import 'package:excelerate/onboarding_screen.dart';
 import 'package:excelerate/splash_screen.dart';
+import 'package:excelerate/login_screen.dart';
+import 'package:excelerate/signup_page.dart';
+import 'package:excelerate/forgot_password_page.dart';
+import 'package:excelerate/home.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-// import 'package:excelerate/home_page.dart';
-// import 'package:excelerate/forgot_password_page.dart';
-// import 'package:excelerate/signup_page.dart';
 
-void main() {
-  
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase with the correct options
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -16,10 +25,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-debugShowCheckedModeBanner: false,
+      title: 'Learn Sphere',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Inter',
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Colors.grey),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Colors.grey),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Colors.blue),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Colors.red),
+          ),
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.all(16),
+        ),
+      ),
       home: const SplashScreen(),
-      routes: {'/onboarding': (context) => const OnboardingScreen()},
+      routes: {
+        '/onboarding': (context) => const OnboardingScreen(),
+        '/login': (context) => const LoginPage(),
+        '/signup': (context) => const SignupPage(),
+        '/forgot-password': (context) => const ForgotPasswordPage(),
+        '/home': (context) => const HomePage(),
+      },
     );
   }
 }
